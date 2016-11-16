19dd9f24211154daa2754d9439f391800aa124fd
rob <robeddy@gmail.com>
2016-11-16 06:02:15 -0500

    Incorporated comments from second submission
    Callback hell.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,8 +1,5 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
+/* feedreader.js */
+
 
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
@@ -27,9 +24,8 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* a test that loops through each feed in the allFeeds object 
+         * and ensures it has a URL defined and that the URL is not empty.
          */
         it('has url defined and url is not empty', function() {
             allFeeds.forEach(function(feed) {
@@ -39,18 +35,8 @@ $(function() {
 
         });
 
-        // it('has url', function() {
-        //     for (var i = 0; i < allFeeds.length; i++) {
-        //         expect(allFeeds[i]['url']).toBeDefined();
-        //         expect(allFeeds[i]['url'].length).not.toBe(0);
-        //     }
-
-        // });
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* a test that loops through each feed in the allFeeds object 
+         * and ensures it has a name defined and that the name is not empty.
          */
         it('has name defined and name is not empty', function() {
             allFeeds.forEach(function(feed) {
@@ -64,13 +50,10 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* a test that ensures the menu element is hidden by default. analyzes 
+         * the HTML and the CSS to determine how we're performing the hiding/showing 
+         * of the menu element.
          */
         var $menuIcon = $('.menu-icon-link');
         var $hiddenClass = $('body').hasClass('menu-hidden');          
@@ -80,9 +63,8 @@ $(function() {
             expect($hiddenClass).toBeTruthy();
         });
 
-         /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
+         /* a test that ensures the menu changes visibility when the menu icon is 
+         * clicked. This test should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
         it('displays when menu item is clicked then hides when clicked again', function() {
@@ -104,11 +86,9 @@ $(function() {
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
         
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+        /* a test that ensures when the loadFeed function is called and 
+         * completes its work there is at least a single .entry element 
+         * within the .feed container. 
          */
         beforeEach(function(done) {
             loadFeed(0, done);
@@ -128,14 +108,32 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        beforeEach(function(done) {
-            loadFeed(1, done);
-        });
 
-        it('has actually changed content', function(done) {
-          expect($('.header .header-title').html()).toBe('CSS Tricks');
-          done();
+        var html1 = '';
+        var html2 = '';
+
+        console.log('here we go');
+        beforeEach(function(done){
+            $('.feed').empty()
+    
+            loadFeed(0, function() {
+                html1 = $('.feed').find("h2").text();
+            });
+    
+            loadFeed(1, function() {
+                html2 = $('.feed').find("h2").text();
+                done();
+            });
         });
+    
+        it('changes the content', function(done){
+            console.log('html1',html1);
+            console.log('html2',html2);            
+            expect(html1).not.toEqual(html2)
+            done();
+        });
+            
+
     });
 
 }());
